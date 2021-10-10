4a9bb8e4d5ef8af791dd97fcbbbe092b09a20404
sebastiangolab <sebagolab97@gmail.com>
2021-10-10 15:33:51 +0200

    Fix modal on ios device

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,38 +1,33 @@
-import React, { createRef, useEffect, createContext } from 'react'
+import React, { createContext } from 'react'
 import ReactDOM from 'react-dom'
 import styled from 'styled-components'
 
-const Wrapper = styled.dialog`
+const BlurWrapper = styled.div`
+    display: ${props => props.isOpen ? 'block' : 'none'};
     position: fixed;
+    left: 0;
+    top: 0;
+    z-index: 99999998;
+    width: 100vw;
+    height: 100vh;
+    backdrop-filter: blur(2px);
+`
+
+const Wrapper = styled.div`
+    position: absolute;
     left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
     padding: 0;
     border: none;
-    z-index: -1;
+    z-index: 99999999;
     max-width: 100%;
     max-height: 100%;
-
-    &::backdrop {
-        width: 100%;
-        height: 100%;
-        backdrop-filter: blur(3px);
-    }
 `
 
 export const ModalContext = createContext()
 
 const Modal = ({ hideModal, isOpen, children }) => {
-
-    const dialogRef = createRef()
-
-    useEffect(
-        () => {
-            isOpen === true
-            ? dialogRef.current.showModal()
-            : dialogRef.current.close()
-        }, [isOpen]
-    )
 
     const hideDialogWhenOutside = e => {
         const rect = e.target.getBoundingClientRect()
@@ -49,14 +44,16 @@ const Modal = ({ hideModal, isOpen, children }) => {
     }
 
     return ReactDOM.createPortal(
-        <Wrapper ref={dialogRef} onClick={hideDialogWhenOutside}>
-            <ModalContext.Provider value={{
-                isModalOpen: isOpen,
-                hideModal
-            }}>
-                {children}
-            </ModalContext.Provider>
-        </Wrapper>,
+        <BlurWrapper isOpen={isOpen}>
+            <Wrapper onClick={hideDialogWhenOutside}>
+                <ModalContext.Provider value={{
+                    isModalOpen: isOpen,
+                    hideModal
+                }}>
+                    {children}
+                </ModalContext.Provider>
+            </Wrapper>
+        </BlurWrapper>,
        document.body
     )
 }
